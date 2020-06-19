95e8c45fb29a811c95a7f414d805a7192eb687ad
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-06-19 16:24:02 +0900

    Update TaskTest.php

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -1,47 +1,5 @@
 <?php
 
-// namespace Tests\Feature;
-
-// use App\Http\Requests\CreateTask;
-// use Carbon\Carbon;
-// use Tests\TestCase;
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-
-// class TaskTest extends TestCase
-// {
-//     use RefreshDatabase;
-
-//     public function setUp() :void
-//     {
-//         parent::setUp();
-//         $this->seed('FoldersTableSeeder');
-//     }
-
-//     public function due_date_should_be_date()
-//     {
-//         $response = $this->post('/folders/1/tasks/create', [
-//             'title' => 'Sample task',
-//             'due_data' => 123,
-//         ]);
-
-//         $response->assertSessionHasErrors([
-//             'due_data' => '期限日には日付を入力してください'
-//         ]);
-//     }
-
-//     public function due_date_should_not_be_past()
-//     {
-//         $response = $this->post('/folders/1/tasks/create', [
-//             'title' => 'Sample task',
-//             'due_data' => Carbon::yesterday()->format('Y/m/d'),
-//         ]);
-
-//         $response->assertSessionHasErrors([
-//             'due_data' => '期限日 には今日以降の日付を入力してください。'
-//         ]);
-//     }
-// } 
-
 namespace Tests\Feature;
 
 use App\Http\Requests\CreateTask;
@@ -67,15 +25,14 @@ class TaskTest extends TestCase
     /**
      * @test
      */
-    public function due_dat_should_be_date()
+    public function due_data_should_be_date()
     {
         $response = $this->post('/folders/1/tasks/create', [
             'title' => 'Sample task',
             'due_data' => 123,
         ]);
-
         $response->assertSessionHasErrors([
-            'due_data' => '期限日 には日付を入力してください。',
+            'due_data' => '期限日には日付を入力してください',
         ]);
     }
 
@@ -91,6 +48,24 @@ class TaskTest extends TestCase
 
         $response->assertSessionHasErrors([
             'due_data' => '期限日 には今日以降の日付を入力してください。',
+        ]);
+    }
+
+    /**
+    * @test
+    */
+    public function status_should_be_within_defined_numbers()
+    {
+        $this->seed('TasksTableSeeder');
+
+        $response = $this->post('/folders/1/tasks/1/edit', [
+            'title' => 'Sample task',
+            'due_date' => Carbon::today()->format('Y/m/d'),
+            'status' => 999,
+        ]);
+    
+        $response->assertSessionHasErrors([
+            'status' => '状態 には 未着手、着手中、完了 のいずれかを指定してください。',
         ]);
     }
 }
