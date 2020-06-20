7b459643893c34c3f4d2bc3144f1daa04a21b9a9
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-06-20 14:23:16 +0900

    auth create

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'HomeController@index')->name('home');
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
 Route::post('/folders/create', 'FolderController@create');
@@ -21,3 +20,6 @@ Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name(
 Route::post('/folders/{id}/tasks/create', 'TaskController@create');
 Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
